c6a15a72c2676c9495007e058bd9623c38ae0bde
aditigawali <aditigawali19@gmail.com>
2024-03-04 18:11:13 +0530

    Added API File

--- app.py
+++ app.py
@@ -43,6 +43,4 @@ def homepage():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
-
-app.run( host= '0.0.0.0', port=config.PORT_NUMBER ,debug=False)
+    app.run( host= '0.0.0.0', port=config.PORT_NUMBER ,debug=False)
